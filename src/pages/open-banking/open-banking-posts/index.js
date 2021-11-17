330c7aee2f29ed55229f5e52f31451c16ad94c3e
Elmira Saifullina <elya@Elmiras-MacBook-Air.local>
2021-11-17 14:38:48 +0100

    changed layout of blog-pages for all available categories

--- src/pages/open-banking/open-banking-posts/index.js
+++ src/pages/open-banking/open-banking-posts/index.js
@@ -1,241 +1,99 @@
 import React from "react"
-import { Link, graphql, navigate } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../../../components/layout"
-import SEO from '../../../components/seo'
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import Breadcrumbs  from "../../../components/breadcrumbs"
+import SEO from "../../../components/seo"
+import Breadcrumbs from "../../../components/breadcrumbs"
+import BlogCardComponent from "../../../components/BlogCardComponent"
 
-const OpenBankingPosts = ({data,location}) => {
-
-  const noStagingPosts = data?data.allStrapiPost.edges.filter(post=>post.node.staging !=true):" ";
-  const postsCategories =[
-    {
-      name:"Open Banking / Open Finance",
-      url:"/open-banking-posts"
-    },
-    {
-      name:"Open Government",
-      url:"/open-government-posts"
-    },
-    {
-      name:"Open Health",
-      url:"/open-health-posts"
-    }
-
-  ]
-  const handleCategory = (selectedCategory)=>{
-    const found = postsCategories.find(cat => cat.name === selectedCategory.name);
-    const goTo = found ? navigate(`${found.url}`) : null
-    return goTo
-  }
-
+const OpenBankingPosts = ({ data, location }) => {
   // const authorsData = data.allStrapiPost.edges[0].node.user;
 
-  return(
-  <Layout>
-    <SEO title="Blog"/>
-    <section className="container mx-auto all-blog-content  px-5">
-      <Breadcrumbs location={location}/>
-      <h3 className="text-3xl font-black text-center">
-        Open Banking / Open Finance 
-      </h3>
-      <h3 className="text-1xl font-black text-center mb-5">Posts and Articles</h3>
-
-      <div className="container mx-auto all-blog-content my-20 px-5">
-        {/* TOP LATESTS 3 POSTS */}
-        <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
-            {data?noStagingPosts.map((post, index) => {
-            
-            while (index < 3 && post.node.staging ===false) {
-              return (
-                <>
-                  <div key={post.node.title} className="px-2 rounded-xl bg-gray-50 shadow pt-5 pb-10 top-blog-cards flex flex-col justify-between">
-                    <div className="top-blog-card-img-container flex justify-center items-center md:h-2/4">
-                      <div className="">
-                      {post.node.featured_image && post.node.featured_image ? (
-                        <Link to={`/${post.node.slug}`} >
-                          {" "}
-                          <GatsbyImage
-                              image={getImage(post.node.featured_image)}
-                              className=""
-                              alt={post.node.title}
-                         
-                            />
-                        </Link>
-                      ) : null}
-                      </div>
-                    </div>
-                    <div className="md:h-2/4">
-                    <Link to={`/${post.node.slug}`}><h5 className="text-lg font-bold leading-5 mt-5">
-                      {post.node.title}
-                    </h5></Link>
-                    <span className="text-gray-600 mr-3 small-text mt-1">
-                      Published{" "}
-                      {new Date(post.node.publishing_date).toDateString()}
-                    </span>{" "}
-                    <div className="text-gray-600 text-sm font-medium flex mb-0 mt-0">
-                      <small className="small-text mr-1">{`Writen by `} </small>
-                      {post.node.user.length === 1 ? (
-                        <Link
-                          className="hover:text-black transition duration-300 ease-in-out small-text mr-1"
-                          to={`/author/${post.node.user[0].id}`}
-                        >{` ${post.node.user[0].username}`}</Link>
-                      ) : post.node.user.length > 1 ? (
-                        post.node.user.map((x, index) => (
-                          <Link
-                            to={`/author/${post.node.user[index].id}`}
-                            className="hover:text-black transition duration-300 ease-in-out small-text mr-1"
-                          >
-                            {x.username}{" "}
-                            {index < post.node.user.length - 1 ? " & " : ""}
-                          </Link>
-                        ))
-                      ) : null}
-                    </div>
-                    <div>
-                      {post.node.categories.map(cat => {
-                        return (
-                          <div key={post.node.id} className="">
-                            <button
-                              to={cat.name}
-                              className={`bg-${cat.name} py-0 px-2 rounded text-white small-text text-xs`}
-                              onClick={()=> handleCategory(cat)}
-                            >
-                              {cat.name}
-                            </button>
-                          </div>
-                        )
-                      })}
-                    </div>
-                    <p className="text-xs leading-5 my-1 small-text ">
-                      {post.node.excerpt ? post.node.excerpt.substr(0,120)+" ..." : ""}
-                    </p>
-                    </div>
-                  </div>
-                </>
-              )
-            }
-          }):null}
-        </div>
-
-        <div className="blog-cards-main-page-container">
-        {data?noStagingPosts.map((post, index) => {
-
-while (index > 2 && post.node.staging ===false) {
-    return (
-<div className="my-4">
-
-        <div className="horizontal-blog-card rounded bg-gray-50 shadow-md">
-      
-          <div className="blog-card-image">
-          <GatsbyImage
-                              image={getImage(post.node.featured_image)}
-                              className=""
-                              alt={post.node.title}
-                            />
-          </div>
-          <div className="blog-card-content">
-          <h5 className="text-lg font-bold leading-5 mt-5">
-                    <Link to={`/${post.node.slug}`}> {post.node.title}</Link>
-          </h5>
-          <span className="text-gray-600 mr-3 small-text mt-1">
-                    Published{" "}
-                    {new Date(post.node.publishing_date).toDateString()}
-          </span>{" "}
-          <div className="text-gray-600 text-sm font-medium flex mb-0 mt-0">
-                    <small className="small-text mr-1">{`Writen by `} </small>
-                    {post.node.user.length === 1 ? (
-                      <Link
-                        className="hover:text-black transition duration-300 ease-in-out small-text mr-1"
-                        to={`/author/${post.node.user[0].id}`}
-                      >{` ${post.node.user[0].username}`}</Link>
-                    ) : post.node.user.length > 1 ? (
-                      post.node.user.map((x, index) => (
-                        <Link
-                          to={`/author/${post.node.user[index].id}`}
-                          className="hover:text-black transition duration-300 ease-in-out small-text mr-1"
-                        >
-                          {x.username}{" "}
-                          {index < post.node.user.length - 1 ? " & " : ""}
-                        </Link>
-                      ))
-                    ) : null}
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <section className="container mx-auto all-blog-content  px-5">
+        <Breadcrumbs location={location} />
+        <h3 className="text-3xl font-black text-center">
+          Open Banking / Open Finance
+        </h3>
+        <h3 className="text-1xl font-black text-center mb-5">
+          Posts and Articles
+        </h3>
+        <div className="container mx-auto all-blog-content my-20 px-5">
+          <div className="container mx-auto all-blog-content px-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+              {data
+                ? data.allStrapiPost.edges.map((post, index) => {
+                    return (
+                      <BlogCardComponent
+                        index={index}
+                        img={post.node.featured_image}
+                        categories={post.node.categories}
+                        publishing_date={post.node.publishing_date}
+                        id={post.node.id}
+                        slug={post.node.slug}
+                        user={post.node.user}
+                        content={post.node.content}
+                        title={post.node.title}
+                      />
+                    )
+                  })
+                : null}
             </div>
-            <div>
-                    {post.node.categories.map(cat => {
-                      return (
-                        <div key={post.node.id} className="">
-                          <button
-                            to={cat.name}
-                            className={`bg-${cat.name} py-0 px-2 rounded text-white small-text text-xs`}
-                            onClick={()=> handleCategory(cat)}
-                          >
-                            {cat.name}
-                          </button>
-                        </div>
-                      )
-                    })}
-                  </div>
-                  <p className="text-xs leading-5 my-1 small-text pr-5">
-                    {post.node.excerpt ? post.node.excerpt : ""}
-                  </p>
           </div>
-     
         </div>
-        
-</div>
-)}
-}):null}
-        </div>
-
-      </div>
-     
-      
-      {/* end of all posts */}
-    </section>
-  </Layout>
-)}
+        {/* end of all posts */}
+      </section>
+    </Layout>
+  )
+}
 
 export default OpenBankingPosts
 
 export const blogQuery = graphql`
-query OpenBankingBlogPosts {
+  query OpenBankingBlogPosts {
     allStrapiPost(
-    sort: {fields: publishing_date, order: DESC}
-    filter: {categories: {elemMatch: {name: {eq: "Open Banking / Open Finance"}}}}
-  ) {
-    edges {
-      node {
-        categories {
-          name
+      sort: { fields: publishing_date, order: DESC }
+      filter: {
+        categories: {
+          elemMatch: { name: { eq: "Open Banking / Open Finance" } }
         }
-        id
-        slug
-        is_featured
-        tags {
-          name
-        }
-        featured_image {
-          childImageSharp {
-            gatsbyImageData(width: 320, blurredOptions: {width: 100}, placeholder: BLURRED)
+      }
+    ) {
+      edges {
+        node {
+          categories {
+            name
           }
-        }
-        title
-        updated_at
-        user {
           id
-          username
+          slug
+          is_featured
+          tags {
+            name
+          }
+          featured_image {
+            childImageSharp {
+              gatsbyImageData(
+                width: 320
+                blurredOptions: { width: 100 }
+                placeholder: BLURRED
+              )
+            }
+          }
+          title
+          updated_at
+
+          user {
+            id
+            username
+          }
+          publishing_date
+          published_at
+          content
+          excerpt
+          staging
         }
-        publishing_date
-        published_at
-        excerpt
-        staging
       }
     }
   }
-}
 `
-
-
-
-
